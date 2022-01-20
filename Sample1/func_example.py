285dc29ea948362d4a5b8387e775b40693c21fae
evyakush <evyakush@gmail.com>
2022-01-21 05:17:27 +1000

    Добавил генератор списков и смешанный вариат генерации матриц ww = [[list(range(x, x+3))] for x in range(3)]

--- Sample1/func_example.py
+++ Sample1/func_example.py
@@ -26,13 +26,18 @@ a = 2 #Добавлено просто, чтобы внести изменени
 
 aa = [x for x in range(0, 2)]
 # print(aa)
-w = [[x for x in (x, x+1)] for x in range(0, 4)]
+w = [[x for x in (x, x, x)] for x in range(0, 4)]
 print(w)
+ex_list = list(range(0,3))
+print(ex_list)
+ww = [[list(range(x, x+3))] for x in range(3)]
 
+print(ww)
 
 if __name__ == '__main__':
     print(simple_func)
     print('Проверка')
+
     print('Проверка 2')
     print('Проверка3')
     print("Проверка4")

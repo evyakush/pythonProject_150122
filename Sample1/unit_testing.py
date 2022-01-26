d9c3ff119f882e5c0530cbde36a916f2acbfeb46
evyakush <evyakush@gmail.com>
2022-01-27 05:46:09 +1000

    Пока разнес все на свои места

--- Sample1/unit_testing.py
+++ Sample1/unit_testing.py
@@ -1,22 +1,18 @@
-a = '$' # expression
+
 allowed = '+-*/' # перечень того, что проверяем
 
 while True:
-    if a in allowed:
-        print('OK')
-    else:
-        print('Ни одного совпадения Not Found')
-    a = input('Введите свой знак')
+    a = input('Введите свой знак, комбинация bp выход из программы')
+
     if a == 'bp':
-        print('Завршил программу')
+        print('Завершил программу')
         break
 
+    if a in allowed:
+        print(f'Ты ввел символ ({a}) он находится в  заданной последовательности, OK')
+    else:
+        print('Ни одного совпадения не найдено')
 
-# for i in allowed: # сам цикл проверки
-#     if a in i:
-#         print('OK')
-#         break
-#     else:
-#         print('No Good')
-#     print('Ни одного совпадения')
-# print('Проверка выполнена')
+
+
+

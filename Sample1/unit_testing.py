827beaed5fb51c26c619f5d1f65fc7f9387eb534
evyakush <evyakush@gmail.com>
2022-01-30 05:12:11 +1000

    Продолжаю прорабатвыать тест из видео: Unit_Test из Python Russian (начало)

--- Sample1/unit_testing.py
+++ Sample1/unit_testing.py
@@ -1,18 +1,33 @@
+#
+# allowed = '+-*/' # перечень того, что проверяем
+#
+# while True:
+#     a = input('Введите свой знак, комбинация bp выход из программы')
+#
+#     if a == 'bp':
+#         print('Завершил программу')
+#         break
+#
+#     if a in allowed:
+#         print(f'Ты ввел символ ({a}) он находится в  заданной последовательности, OK')
+#     else:
+#         print('Ни одного совпадения не найдено')
+#
+# 
+from functools import reduce
+#
+# sizes = input('Введите три значения')
+# print(sizes)
+# print(type(sizes))
+# sizes = sizes.split()
+# print(type(sizes))
+# print(sizes)
+# x, y, zx, y, z = map(int, input('Введите еще три значения').strip().split())
+# print(f'{x=} , {y = }, {z= }')
 
-allowed = '+-*/' # перечень того, что проверяем
-
-while True:
-    a = input('Введите свой знак, комбинация bp выход из программы')
-
-    if a == 'bp':
-        print('Завершил программу')
-        break
-
-    if a in allowed:
-        print(f'Ты ввел символ ({a}) он находится в  заданной последовательности, OK')
-    else:
-        print('Ни одного совпадения не найдено')
-
-
-
+volume_reduce = reduce(lambda x, y: x*y, map(int, input('Введите еще три значения').strip().split()))
+print(volume_reduce)
+names_list = ["Иван", "Прохор", "Илья", "Анастасия", "Альбина"]
+name_start_letter =[nm_letter for nm_letter in names_list if nm_letter.startswith("П")]
+print(name_start_letter)
 

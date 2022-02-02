1b9a96b1a334bad32da499e96c3def95c7da86f2
evyakush <evyakush@gmail.com>
2022-02-03 05:01:08 +1000

    Пытаюсь исползовать assert Вставлена функция которую тест должен забраковать - делление на ноль

--- Sample1/assert_example.py
+++ Sample1/assert_example.py
@@ -1,5 +1,16 @@
-a = 1
-b = 0
-b = b + 1
-assert b != 0, "Деление на ноль недопустимо"
-print('Continue')
+
+# def sterngth_pass(value):
+#     if len(value) < 8:
+#         print('from function')
+#         return "weak pass"
+#     return 'Print'
+def divide_num(a,b):
+    try:
+        aa = a/b
+        return aa
+    except ZeroDivisionError:
+        # print("Деление на ноль")
+        return 'Divide by zero'
+if __name__ == '__main__':
+    # assert sterngth_pass(' ')
+    print(divide_num(1,0))
